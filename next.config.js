b66f12e4d317f6ef2471b2c583cd2eb23ae85efe
allemain100 <55.linage.mitoses@icloud.com>
2025-06-01 20:28:23 +0000

    gh pages begin

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 const withMDX = require("@next/mdx")();
 
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+  reactStrictMode: false,
+  trailingSlash: true,
+  assetPrefix: isProd ? '/<your-repository-name>/' : '',
+  basePath: isProd ? '/<your-repository-name>' : '',
+  output: 'export',
 };
+
+// module.exports = nextConfig
 
 module.exports = withMDX(nextConfig);
